185e4d91d3af2e2fbb923e8de95794fc480428ec
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-26 17:46:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+num = (1..20).to_a
+
+def prime?(n)
+  i = 2 
+  
+  while n > i
+    if n % i == 0
+      puts "Not a prime"
+      break
+    elsif n == i
+      puts "IT IS A PRIME"
+    end
+  end
+  
+end 
+
+puts prime?(3)
+
+#returns a boolean whenever the number is a prime number or not
+  # a prime number n has only two factors 1 and n (itself)
+  
+  # expect(prime?(2)).to be(true)
